008446846162b337190fa44277158f13516208e7
burgerax5 <burgerax3@hotmail.com>
2024-01-03 21:02:41 +1300

    Created budgetService and its unit tests

--- backend/src/services/budgetService.ts
+++ backend/src/services/budgetService.ts
@@ -2,6 +2,12 @@ import { Budget } from "../models/Budget"
 import { User } from "../models/User"
 import { Category } from "../models/Category"
 
+export interface budgetServiceReturn {
+    success: boolean,
+    budget?: Budget,
+    error?: string
+}
+
 export class BudgetServices {
     private budgets: Budget[]
     private next_budget_id
@@ -11,48 +17,66 @@ export class BudgetServices {
         this.next_budget_id = 0
     }
 
-    addMonthlyBudget(user: User, budget_details: {
+    getAllBudgets(): Budget[] {
+        return this.budgets
+    }
+
+    getBudgetByUser(user: User): Budget[] {
+        return this.budgets.filter(budget => budget.user === user)
+    }
+
+    getBudgetById(budget_id: number): Budget | undefined {
+        return this.budgets.find(budget => budget.budget_id === budget_id)
+    }
+
+    checkBudgetEmpty(user: User, category: Category | undefined, month: number | undefined, year: number): boolean {
+        // If there isn't already a budget set for the specified the category & date for the user
+        return this.budgets.some(budget => {
+            if (category) {
+                return budget.budget_month === month 
+                && budget.budget_year === year 
+                && budget.user === user
+                && budget.category === category
+            } return false
+        })
+    }
+
+    addBudget(user: User, budget_details: {
         category: Category | undefined,
         amount: number,
         budget_month: number | undefined,
         budget_year: number
-    }) {
+    }): budgetServiceReturn {
+        const { category, amount, budget_month, budget_year } = budget_details
+
         const new_budget: Budget = {
             budget_id: this.next_budget_id++,
             user,
-            category,
-            amount,
-            budget_month,
-            budget_year
+            ...budget_details
         }
 
-        this.budgets.push(new_budget)
+        // Make sure there isn't a budget for the user with the same date & category
+        if (!this.checkBudgetEmpty(user, category, budget_month, budget_year)) {
+            this.budgets.push(new_budget)
+            return { success: true, budget: new_budget }
+        } return { success: false, error: "Budget not found" }
+    }
+
+    editBudget(budget_id: number, new_amount: number): budgetServiceReturn {
+        const budget = this.getBudgetById(budget_id)
+
+        if (budget) {
+            budget.amount = new_amount
+            return { success: true, budget: budget }
+        } return { success: false, error: `No budget with the id ${budget_id}` }
+    }
+
+    deleteBudget(budget_id: number): budgetServiceReturn {
+        const budget = this.getBudgetById(budget_id)
+
+        if (budget) {
+            this.budgets = this.budgets.filter(b => b.budget_id !== budget_id)
+            return { success: true }
+        } return { success: false, error: `No budget with the id ${budget_id}` }
     }
 }
-
-
-// import { Category } from "../models/Category";
-
-// export class CategoryServices {
-//     private categories: Category[]
-
-//     constructor() {
-//         this.categories = [
-//             {category_id: 0, name: "Food & Drink", colour: "#f5b642"},
-//             {category_id: 1, name: "Entertainment", colour: "#f54e42"},
-//             {category_id: 2, name: "Transportation", colour: "#dd42f5"},
-//             {category_id: 3, name: "Health", colour: "#54f542"},
-//             {category_id: 4, name: "Groceries", colour: "#ba4111"},
-//             {category_id: 5, name: "Education", colour: "#11bab2"},
-//             {category_id: 6, name: "Housing", colour: "#ba115a"}
-//         ]
-//     }
-
-//     public getAllCategories(): Category[] {
-//         return this.categories
-//     }
-
-//     public getCategoryByName(category_name: string): Category | undefined {
-//         return this.categories.find(category => category.name === category_name)
-//     }
-// }
--- backend/tests/unit/budget.test.ts
+++ backend/tests/unit/budget.test.ts
@@ -1,12 +1,169 @@
-import { BudgetServices } from "../../src/services/budgetService";
+import { BudgetServices, budgetServiceReturn } from "../../src/services/budgetService";
 import { Budget } from "../../src/models/Budget";
 import { User } from "../../src/models/User";
 import { UserService } from "../../src/services/userService";
 import { Category } from "../../src/models/Category";
 import { CategoryServices } from "../../src/services/categoryService";
+import { jestRegister } from "../registerUser";
 
-describe('Test to add a monthly budget', () => {
+describe('Test adding budgets', () => {
+    let userService: UserService
+    let categoryService: CategoryServices
+    let budgetService: BudgetServices
+    let user: User
+
+    beforeEach(async () => {
+        userService = new UserService()
+        categoryService = new CategoryServices()
+        budgetService = new BudgetServices()
+        user = userService.getAllUsers()[0]
+        await jestRegister('bob', 'password123', userService)
+    })
+
     it('should add a monthly budget', () => {
-        
+        const budget_details = {
+            category: categoryService.getCategoryByName('Entertainment'),
+            amount: 49.99,
+            budget_month: undefined,
+            budget_year: 2023
+        }
+
+        budgetService.addBudget(user, budget_details)
+        const budgets = budgetService.getAllBudgets()
+
+        expect(budgets.length).toBe(1)
+        expect(budgets[0].user).toBe(user)
+    })
+
+    it('should not allow a user to have multiple budgets for the same category on the same date', () => {
+        const budget1_details = {
+            category: categoryService.getCategoryByName('Entertainment'),
+            amount: 49.99,
+            budget_month: 2,
+            budget_year: 2023
+        }
+
+        const budget2_details = { ...budget1_details, amount: 99.99 }
+
+        // Only the first budget added will be considered
+        const isAdded1 = budgetService.addBudget(user, budget1_details).success
+        const isAdded2 = budgetService.addBudget(user, budget2_details).success
+
+        const all_budgets: Budget[] = budgetService.getAllBudgets()
+
+        expect(all_budgets.length).toBe(1)
+        expect(isAdded1).toBeTruthy()
+        expect(isAdded2).toBeFalsy()
+    })
+})
+
+describe('Test editing & deleting budgets for a user', () => {
+    let userService: UserService
+    let categoryService: CategoryServices
+    let budgetService: BudgetServices
+    let user: User
+
+    beforeEach(async () => {
+        userService = new UserService()
+        categoryService = new CategoryServices()
+        budgetService = new BudgetServices()
+        user = userService.getAllUsers()[0]
+        await jestRegister('bob', 'password123', userService)
+    })
+
+    it('should modify the price of the budget', () => {
+        const budget1 = {
+            category: categoryService.getCategoryByName('Entertainment'),
+            amount: 49.99,
+            budget_month: 2,
+            budget_year: 2023
+        }
+
+        const { success, budget } = budgetService.addBudget(user, budget1)
+
+        expect(budget?.budget_id).not.toBeUndefined()
+
+        if (budget?.budget_id !== undefined) {
+            budgetService.editBudget(budget?.budget_id, 99.99)
+        }
+
+        expect(budget?.amount).toBe(99.99)
+    })
+
+    it('should return an error saying that the there is no budget with id 1', () => {
+        const { success, error, budget } = budgetService.editBudget(1, 50.00)
+
+        expect(success).toBeFalsy()
+        expect(error).toBe("No budget with the id 1")
+        expect(budget).toBeUndefined()
+    })
+
+    it('should remove the budget with id of 0', () => {
+        const budget1_details = {
+            category: categoryService.getCategoryByName('Entertainment'),
+            amount: 49.99,
+            budget_month: 2,
+            budget_year: 2023
+        }
+
+        const { success, budget } = budgetService.addBudget(user, budget1_details)
+        expect(success).toBeTruthy()
+        expect(budget?.budget_id).toBe(0)
+
+        if (budget) {
+            const { success: deleteSuccess } = budgetService.deleteBudget(budget?.budget_id) 
+            expect(deleteSuccess).toBeTruthy()
+        }
+    })
+
+    it('should return an error when we try deleting a budget with id 1', () => {
+        const { success: deleteSuccess } = budgetService.deleteBudget(1) 
+        expect(deleteSuccess).toBeFalsy()
+    })
+})
+
+describe('Test getting budgets by user', () => {
+    let userService: UserService
+    let categoryService: CategoryServices
+    let budgetService: BudgetServices
+    let user: User
+
+    beforeEach(async () => {
+        userService = new UserService()
+        categoryService = new CategoryServices()
+        budgetService = new BudgetServices()
+        user = userService.getAllUsers()[0]
+        await jestRegister('bob', 'password123', userService)
+    })
+
+    it('should return all budgets created by the user "alice"', () => {
+        const budget_detail_template = {
+            category: categoryService.getCategoryByName('Entertainment'),
+            amount: 49.99,
+            budget_month: 2,
+            budget_year: 2023
+        }
+    
+        budgetService.addBudget(user, budget_detail_template)
+        budgetService.addBudget(user, {...budget_detail_template, budget_year: 2022})
+        budgetService.addBudget(user, {...budget_detail_template, budget_year: 2021})
+
+        const budgets = budgetService.getBudgetByUser(user)
+        expect(budgets.length).toBe(3)
+    })
+
+    it('should return false since the user already has a budget for that category on that date', () => {
+        const budget_detail_template = {
+            category: categoryService.getCategoryByName('Entertainment'),
+            amount: 49.99,
+            budget_month: 2,
+            budget_year: 2023
+        }
+
+        const { category, budget_month, budget_year } = budget_detail_template
+    
+        budgetService.addBudget(user, budget_detail_template)
+        const isExist = budgetService.checkBudgetEmpty(user, category, budget_month, budget_year)
+        expect(isExist).toBeTruthy()
     })
 })
