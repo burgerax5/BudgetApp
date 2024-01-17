6e1038a5ee8cf194a9642bf42b97b2ed7a0768a6
burgerax5 <burgerax3@hotmail.com>
2024-01-17 21:22:34 +1300

    Fixed theme changing after switching page

--- frontend/src/components/ModeToggle.tsx
+++ frontend/src/components/ModeToggle.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 import { Moon, Sun } from "lucide-react"
- 
+
 import { Button } from "@/components/ui/button"
 import {
   DropdownMenu,
@@ -8,25 +8,28 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
- 
+
 export function ModeToggle() {
   const [theme, setThemeState] = React.useState<
     "theme-light" | "dark" | "system"
   >("theme-light")
- 
+
   React.useEffect(() => {
-    const isDarkMode = document.documentElement.classList.contains("dark")
+    const isDarkMode = typeof localStorage !== undefined && localStorage.getItem("theme") === "dark"
+    document.documentElement.classList[isDarkMode ? "add" : "remove"]("dark");
     setThemeState(isDarkMode ? "dark" : "theme-light")
   }, [])
- 
+
   React.useEffect(() => {
     const isDark =
       theme === "dark" ||
       (theme === "system" &&
         window.matchMedia("(prefers-color-scheme: dark)").matches)
+
     document.documentElement.classList[isDark ? "add" : "remove"]("dark")
+    localStorage.setItem('theme', isDark ? "dark" : "light")
   }, [theme])
- 
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
