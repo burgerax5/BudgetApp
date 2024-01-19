77dfd6942aec71c5f9197c5e2003cff7771d20d9
burgerax5 <burgerax3@hotmail.com>
2024-01-20 12:41:52 +1300

    Different home page for guests & logged in users

---
+++ frontend/src/components/Dashboard.tsx
@@ -0,0 +1,37 @@
+import { DialogButton } from "@/components/ExpenseDialog";
+import Calendar from "@/components/Calendar";
+
+import Budget from "@/components/Budget";
+import CategoriesCard from "@/components/CategoriesCard";
+
+function Dashboard() {
+    return (
+        <>
+            {/* <!-- Top --> */}
+            <div
+                className="flex items-center mt-3 flex-col justify-center gap-3 sm:flex-row sm:justify-between"
+            >
+                <h1 className="text-center sm:text-left text-2xl font-bold">
+                    Dashboard
+                </h1>
+                <div className="flex items-center gap-3.5">
+                    <Calendar />
+                    <DialogButton />
+                </div>
+            </div>
+
+            <div className="flex flex-col sm:flex-row mt-5">
+                {/* <!-- Left side --> */}
+                <div className="flex flex-col gap-3.5 w-96">
+                    <Budget />
+                    <CategoriesCard />
+                </div>
+
+                {/* <!-- Right side --> */}
+                <div></div>
+            </div>
+        </>
+    )
+}
+
+export default Dashboard
