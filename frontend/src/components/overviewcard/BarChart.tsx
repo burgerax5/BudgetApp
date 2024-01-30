a347e07b78dea08d3cd472a06eddf9c2911d78d5
burgerax5 <burgerax3@hotmail.com>
2024-01-30 19:02:13 +1300

    Starting on bar chart in dashboard

---
+++ frontend/src/components/overviewcard/BarChart.tsx
@@ -0,0 +1,29 @@
+import { Bar } from "react-chartjs-2"
+import { Chart as ChartJS } from "chart.js/auto"
+
+interface Dataset {
+    label: string,
+    data: number[],
+    backgroundColor: string
+}
+
+interface ExpenseData {
+    labels: number[],
+    datasets: Dataset[]
+}
+
+interface Props {
+    expenseData: ExpenseData
+}
+
+const BarChart: React.FC<Props> = ({ expenseData }) => {
+    console.log(expenseData)
+
+    return (
+        <div className="flex flex-col gap-1">
+            {expenseData.labels.length && <Bar data={expenseData} />}
+        </div>
+    )
+}
+
+export default BarChart
--- frontend/src/components/overviewcard/OverviewCard.tsx
+++ frontend/src/components/overviewcard/OverviewCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import {
     Card,
     CardContent,
@@ -8,9 +8,100 @@ import {
 } from "@/components/ui/card"
 import { useStore } from '@nanostores/react'
 import { selectedDate } from '@/store/userStore'
+import BarChart from './BarChart'
+import axios from '@/api/axios'
+
+interface Dataset {
+    label: string,
+    data: number[],
+    backgroundColor: string
+}
+
+interface Expense {
+    id: number,
+    userId: number,
+    categoryId: number,
+    currencyId: number,
+    name: string,
+    amount: number
+}
+
+interface ExpenseData {
+    labels: number[],
+    datasets: Dataset[],
+}
+
+enum Month {
+    January = 1,
+    February,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December
+}
 
 const OverviewCard = () => {
     const $selectedDate = useStore(selectedDate)
+    const [labels, setLabels] = useState<number[]>([])
+    const [data, setData] = useState<number[]>([])
+    const [expenseData, setExpenseData] = useState<ExpenseData>({
+        labels: labels,
+        datasets: [{
+            label: "Amount spent",
+            data: data,
+            backgroundColor: "hsl(222.2, 47.4%, 11.2%)"
+        }]
+    })
+
+    const daysInMonth = (year: number, month: number) => {
+        const lastDayOfMonth = new Date(year, month, 0)
+        return lastDayOfMonth.getDate()
+    }
+
+    useEffect(() => {
+        const getExpenses = async () => {
+            const month = $selectedDate.date.getMonth() + 1
+            const year = $selectedDate.date.getFullYear()
+            const numberOfDays = daysInMonth(year, month)
+            let days: number[] = []
+
+            for (let i = 1; i <= ($selectedDate.yearOnly ? 12 : numberOfDays); i++)
+                days.push(i)
+
+            const url = $selectedDate.yearOnly ? `/expense/?year=${year}` : `/expense/?month=${month}&year=${year}`
+
+            await axios.get(url, { withCredentials: true })
+                .then(res => {
+                    const expenses: Expense[] = res.data.expenses
+                    setLabels(days)
+                    setData(expenses.map(expense => expense.amount))
+                })
+                .catch(err => {
+                    console.error('Error occurred while getting expenses for the month:', err)
+                })
+        }
+
+        getExpenses()
+    }, [$selectedDate])
+
+    useEffect(() => {
+        setExpenseData({
+            labels: labels,
+            datasets: [{
+                label: "Amount spent",
+                data: data
+            }]
+        })
+
+    }, [labels, data])
+
+    console.log(expenseData)
 
     return (
         <Card>
@@ -19,7 +110,7 @@ const OverviewCard = () => {
                 <CardDescription>Your spendings this year</CardDescription>
             </CardHeader>
             <CardContent>
-                PP
+                <BarChart expenseData={expenseData} />
             </CardContent>
         </Card>
     )
